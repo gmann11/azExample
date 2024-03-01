8d62e517fecb8ea9c93be3739741e79673944aa9
Gary Mann <gmann11@users.noreply.github.com>
2024-03-01 05:21:40 -0800

    http api test

--- src/test/java/org/neo4j/field/azex/TestAzEx.java
+++ src/test/java/org/neo4j/field/azex/TestAzEx.java
@@ -1,5 +1,11 @@
 package org.neo4j.field.azex;
 
+import com.squareup.okhttp.Credentials;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
 import java.util.Map;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -13,16 +19,17 @@ import org.neo4j.driver.Result;
 import org.neo4j.driver.Session;
 
 public class TestAzEx {
-
-    final static String uri = "bolt://localhost:7687";
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    final static String bolturl = "bolt://localhost:7687";
     final static String user = "neo4j";
     final static String pwd = "password";
     private static Driver driver = null;
+    private static String httpurl = "http://localhost:7474/db/neo4j/tx";
     
     @BeforeClass
     public static void init() throws Exception {
         try {
-            driver = GraphDatabase.driver(uri, AuthTokens.basic(user, pwd));
+            driver = GraphDatabase.driver(bolturl, AuthTokens.basic(user, pwd));
             driver.verifyConnectivity();
         } catch (Exception e) {
             e.printStackTrace();
@@ -30,7 +37,7 @@ public class TestAzEx {
     }
     
     @Test
-    public void testAz() {
+    public void testAzBolt() {
         Map<String, Object> params = Map.of("jparam1", "value1", "jparam2", "value2");
         AuthToken at = AuthTokens.custom(user, pwd, null, "basic", params);
         try {
@@ -44,7 +51,25 @@ public class TestAzEx {
         }
     }
     
-    
-    
-    
+    @Test
+    public void testAzHttp() {
+        OkHttpClient client = new OkHttpClient();
+        String json = "{\"statements\":[{\"statement\":\"RETURN true\"}]}";
+        RequestBody body = RequestBody.create(JSON, json);
+        String cred = Credentials.basic(user, pwd);
+        Request request = new Request.Builder()
+                .addHeader("Content-type", "application/json")
+                .addHeader("Authorization", cred)
+                .url(httpurl)
+                .post(body)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String rs = response.body().string();
+            assertTrue(rs.contains("[true]"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            assertFalse(true);
+        }
+    }
 }
